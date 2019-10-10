a982dd50ca1b5198cdc951ae0ac199abae854926
daniel.pal <pdany86@gmail.com>
2019-10-11 00:33:02 +0200

    GyroControl: targeting

--- GyroControl/gyro_pid.cs
+++ GyroControl/gyro_pid.cs
@@ -1,55 +1,27 @@
-public Program() { Runtime.UpdateFrequency = UpdateFrequency.Update1; }
-
-void printVector(IMyTextPanel panel, string prefix, Vector3D v)
-{
-	var str = "";
-	str += prefix + " ";
-	str += v.X.ToString("0.00") + ", ";
-	str += v.Y.ToString("0.00") + ", ";
-	str += v.Z.ToString("0.00");
-	str += "\r\n";
-	panel.WriteText(str, true);
-}
-
 IMyRemoteControl rc = null;
-IMyGyro gyro = null;
-IMyTextPanel lcd = null;
-List<IMyThrust> thrusterList = new List<IMyThrust>();
+List<IMyGyro> gyros = new List<IMyGyro>();
+List<IMyThrust> thrusterUpList = new List<IMyThrust>();
+List<IMyRemoteControl> remoteControls = new List<IMyRemoteControl>();
+IMyTerminalBlock pidStoreObject = null;
 
-void printVelocities()
-{
-	var v = rc.GetShipVelocities();
-	printVector(lcd, "V", v.LinearVelocity);
-	printVector(lcd, "aV", v.AngularVelocity);
+public Program() {
+	Runtime.UpdateFrequency = UpdateFrequency.Update1;
+	
+	GridTerminalSystem.GetBlocksOfType<IMyRemoteControl>(remoteControls);
+	GridTerminalSystem.GetBlocksOfType<IMyThrust>(thrusterUpList);
+	GridTerminalSystem.GetBlocksOfType<IMyGyro>(gyros);
+	pidStoreObject = (IMyTerminalBlock)gyros[0];
+	
+	if(remoteControls.Count>0) rc = remoteControls[0];
 }
 
-void printOrientation()
-{
-	Vector3D pF,pU,pR;
-	getOrientationVectors(out pF,out pU,out pR);
-	printVector(lcd, "F", pF);
-	printVector(lcd, "R", pR);
-	printVector(lcd, "U", pU);
-	double roll, pitch;
-	getRollPitchGravity(out roll, out pitch);
-	roll *= 180.0/Math.PI;
-	pitch *= 180.0/Math.PI;
-	lcd.WriteText("roll, pitch: " + roll.ToString("0.000") + ", " + pitch.ToString("0.000") + "\r\n",true);
-}
+float trim(float val,float th) {if(val<-th) return -th; if(val>th) return th; return val;}
 
 void getOrientationVectors(out  Vector3D F, out Vector3D U, out Vector3D R) {
-    var pOrig = Me.CubeGrid.GridIntegerToWorld(new Vector3I(0, 0, 0));
-	var pF = Me.CubeGrid.GridIntegerToWorld(new Vector3I(1, 0, 0)) - pOrig;
-	var pR = Me.CubeGrid.GridIntegerToWorld(new Vector3I(0, 0, 1)) - pOrig;
-	var pU = Me.CubeGrid.GridIntegerToWorld(new Vector3I(0, 1, 0)) - pOrig;
-	pF.Normalize();
-	pR.Normalize();
-	pU.Normalize();
-    F = pF;
-    R = pR;
-    U = pU;
+	F = rc.WorldMatrix.Forward;
+	R = rc.WorldMatrix.Right;
+	U = rc.WorldMatrix.Up;
 }
-
 
 void getRollPitch(out double roll, out double pitch,Vector3D vRef) {
 	Vector3D F,U,R;
@@ -69,19 +41,18 @@ void getRollPitchGravity(out double roll, out double pitch) {
 	getRollPitch(out roll, out pitch,gravUpUnit);
 }
 
-float trim(float val,float th) {if(val<-th) return -th; if(val>th) return th; return val;}
-
-void updateGyro(Vector3D vRef, float dt = 0.016f, float targetRoll = 0.0f, float targetPitch = 0.0f) {
+Vector3D calcGyroControl(Vector3D vRef, float dt = 0.016f, float targetRoll = 0.0f, float targetPitch = 0.0f) {
 	float kP = 2.0f;
-	float kI = 0.05f;
-	float kD = 5.0f;
+	float kI = 0.2f;
+	float kD = 4.0f;
 	
 	float lastR = 0.0f;
 	float lastP = 0.0f;
 	float integR = 0.0f;
 	float integP = 0.0f;
-	if(gyro.CustomData!="") {
-		var list = gyro.CustomData.Split(',');
+	
+	if(pidStoreObject.CustomData!="") {
+		var list = pidStoreObject.CustomData.Split(',');
 		lastR = float.Parse(list[0]);
 		lastP = float.Parse(list[1]);
 		// no Yaw
@@ -89,7 +60,7 @@ void updateGyro(Vector3D vRef, float dt = 0.016f, float targetRoll = 0.0f, float
 		integP = float.Parse(list[4]);
 		// no Yaw
 	} else {
-		gyro.CustomData = "0.0,0.0,0.0,0.0,0.0,0.0";
+		pidStoreObject.CustomData = "0.0,0.0,0.0,0.0,0.0,0.0";
 	}
 
 	double _roll, _pitch;
@@ -112,72 +83,105 @@ void updateGyro(Vector3D vRef, float dt = 0.016f, float targetRoll = 0.0f, float
 	integP = trim(integP,2.0f);
 
 	string storeInfo = roll.ToString() + "," + pitch.ToString() + ",0.0," + integR + "," + integP + ",0.0";
-	gyro.CustomData = storeInfo;
+	pidStoreObject.CustomData = storeInfo;
 
 	float controlR = kP*eR + kD*dR + kI*integR;
 	float controlP = kP*eP + kD*dP + kI*integP;
-	controlP *= -1;
-	controlR *= -1;
 	//controlR = trim(controlR,10.0f);
 	//controlP = trim(controlP,10.0f);
-	gyro.Roll = controlR;
-	gyro.Pitch = controlP;
-
-	if(lcd != null) {
-		lcd.WriteText("PID: "+  kP.ToString("0.0000") + ", "+ kI.ToString("0.0000") + ", " + kD.ToString("0.0000") + "\r\n",true);
-		lcd.WriteText("Err: "+ eR.ToString("0.00") + ", "+ eP.ToString("0.00") + "\r\n",true);
-		lcd.WriteText("dErr: "+ dR.ToString("0.00") + ", "+ dP.ToString("0.00") + "\r\n",true);
-		lcd.WriteText("Int: "+ integR.ToString("0.00") + ", "+ integP.ToString("0.00") + "\r\n",true);
-		lcd.WriteText("Ctrl: "+ (controlR*180.0/Math.PI).ToString("0.00") + ", "+ (controlP*180.0/Math.PI).ToString("0.00") + "\r\n",true);
-	}
+	return new Vector3D(controlR, controlP, 0.0);
 }
+
+public enum PathType {
+	Direct,
+	UpTravelTarget,
+	UpTravelBallisticRelease
+};
+
+PathType pathType = PathType.UpTravelTarget;
 
 void updateToTarget(Vector3D targetPos)
 {
+	// Me
+	var v_av = rc.GetShipVelocities();
+	var v = v_av.LinearVelocity;
+	var curSpeed = v.Length();
+	var curPos = rc.GetPosition();
+	
+	// gravity
 	var grav = rc.GetTotalGravity();
 	var gravUpUnit = -1*grav;
 	gravUpUnit.Normalize();
 	
-	var v_av = rc.GetShipVelocities();
-	var v = v_av.LinearVelocity;
-	var vTarget = targetPos - rc.GetPosition();
-	
-	double targetDist = vTarget.Length();
-	double h = gravUpUnit.Dot(vTarget);
-	double surfaceDist = Math.Sqrt(targetDist*targetDist - h*h);
-	
-	
-	
-	vTarget.Normalize();
-	vTarget *= 100.0;
-	
-	vTarget += surfaceDist/100 * gravUpUnit;
-	
+	// target
+	var targetRelPos = targetPos - curPos;
+	double targetDist = targetRelPos.Length();
+	double vertDist = -1 * gravUpUnit.Dot(targetRelPos);
+	double horizDist = Math.Sqrt(targetDist*targetDist - vertDist*vertDist);
+	var vertVec = vertDist * gravUpUnit;
+	var horizVec = vertVec + targetRelPos;
+
+	// path / target velocity selection
+	var vTarget = new Vector3D();
+	switch(pathType)
+	{
+		case PathType.UpTravelTarget:
+		{
+			double heightTarget = 4000.0;
+			if(horizDist > 2000.0) {
+				var heightTargetVec = (heightTarget - vertDist) * gravUpUnit + 0.05 * horizVec;
+				vTarget = heightTargetVec;
+			} else if(true) {
+				vTarget = targetRelPos;
+			}
+			vTarget.Normalize();
+			vTarget *= 150.0;
+		}
+		break;
+		
+		case PathType.Direct:
+		default:
+		{
+			vTarget = targetRelPos;
+			vTarget.Normalize();
+			vTarget *= 150.0;
+		}
+		break;
+	}
+
+	// target direction to achieve target velocity
 	var vDiff = vTarget - v;
 	var vRef = vDiff;
 	
-	GridTerminalSystem.GetBlocksOfType<IMyThrust>(thrusterList);
-
-	for(int i=0;i<thrusterList.Count;i++) {
-		thrusterList[i].ThrustOverridePercentage = 1.0f;
+	// gyro control
+	var controlRP = calcGyroControl(vRef);
+	
+	controlRP *= 1.0;
+	
+	for(int i=0;i<gyros.Count;i++) {
+		gyros[i].GyroOverride = true;
+		gyros[i].Roll = (float)controlRP.X;
+		gyros[i].Pitch = (float)controlRP.Y;
 	}
 	
-	updateGyro(vRef);
-}
+	// thruster control
+	//double cosV = v.Dot();
+	
+	float thrusterOverrideVal = 1.0f;
+	//if(curSpeed > 95.0) thrusterOverrideVal = 0.1f;
 
+	for(int i=0;i<thrusterUpList.Count;i++) {
+		thrusterUpList[i].ThrustOverridePercentage = thrusterOverrideVal;
+	}
+}
 
 public void Main(string argument, UpdateType updateSource)
 {
-	rc = GridTerminalSystem.GetBlockWithName("RC.Main") as IMyRemoteControl;
-	gyro = GridTerminalSystem.GetBlockWithName("Gyroscope") as IMyGyro;
-	lcd = GridTerminalSystem.GetBlockWithName("LCD") as IMyTextPanel;
-	lcd.WriteText("");
-	printVelocities();
-	printOrientation();
+	
 	updateToTarget(new Vector3D(54804.56,-27195.05,11812.88));
 	//53538.92, -26695.54, 12130.25
+	// Enemy_54474.60,-26995.93,7156.81
 	
 	//rc.ControlThrusters = false;
 	//rc.DampenersOverride = false;
 }
-
