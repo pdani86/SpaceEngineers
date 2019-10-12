059fc343572ca0d6cf8f98491044cf3a9bf803c3
daniel.pal <pdany86@gmail.com>
2019-10-13 00:53:46 +0200

    GyroControl: PID_Control class

--- GyroControl/gyro_pid.cs
+++ GyroControl/gyro_pid.cs
@@ -1,21 +1,46 @@
+double trim(double val,double th) {if(val<-th) return -th; if(val>th) return th; return val;}
+
+class PID_Control
+{
+	public double kP = 2.0;
+	public double kI = 0.5;
+	public double kD = 4.0;
+	
+	public double integ = 0.0;
+	public double lastVal = 0.0;
+	public double lastErr = 0.0;
+	public double control = 0.0;
+	
+	double trim(double val,double th) {if(val<-th) return -th; if(val>th) return th; return val;}
+	
+	public double update(double signal, double target, double dt = 0.016666) {
+		double err = target - signal;
+		integ += err * dt;
+		double d = (err - lastErr)/dt;
+		integ = trim(integ,2.0f);
+		control = kP*err + kD*d + kI*integ;
+		lastVal = signal;
+		lastErr = err;
+		//control = trim(control,10.0f);
+		return control;
+	}
+};
+
 IMyRemoteControl rc = null;
 List<IMyGyro> gyros = new List<IMyGyro>();
 List<IMyThrust> thrusterUpList = new List<IMyThrust>();
 List<IMyRemoteControl> remoteControls = new List<IMyRemoteControl>();
-IMyTerminalBlock pidStoreObject = null;
+PID_Control pidRoll = new PID_Control();
+PID_Control pidPitch = new PID_Control();
+//PID_Control pidThrusters = new PID_Control();
 
 public Program() {
 	Runtime.UpdateFrequency = UpdateFrequency.Update1;
-	
 	GridTerminalSystem.GetBlocksOfType<IMyRemoteControl>(remoteControls);
 	GridTerminalSystem.GetBlocksOfType<IMyThrust>(thrusterUpList);
 	GridTerminalSystem.GetBlocksOfType<IMyGyro>(gyros);
-	pidStoreObject = (IMyTerminalBlock)gyros[0];
-	
 	if(remoteControls.Count>0) rc = remoteControls[0];
 }
-
-float trim(float val,float th) {if(val<-th) return -th; if(val>th) return th; return val;}
 
 void getOrientationVectors(out  Vector3D F, out Vector3D U, out Vector3D R) {
 	F = rc.WorldMatrix.Forward;
@@ -30,8 +55,8 @@ void getRollPitch(out double roll, out double pitch,Vector3D vRef) {
 
 	double cosFG = F.Dot(vRef);
 	double cosRG = R.Dot(vRef);
-	pitch = Math.Acos(cosFG) - Math.PI/2.0;
-	roll = Math.Acos(cosRG) - Math.PI/2.0;
+	pitch = Math.PI/2.0 - Math.Acos(cosFG);
+	roll = Math.PI/2.0 - Math.Acos(cosRG);
 }
 
 void getRollPitchGravity(out double roll, out double pitch) {
@@ -42,51 +67,12 @@ void getRollPitchGravity(out double roll, out double pitch) {
 }
 
 Vector3D calcGyroControl(Vector3D vRef, float dt = 0.016f, float targetRoll = 0.0f, float targetPitch = 0.0f) {
-	float kP = 2.0f;
-	float kI = 0.2f;
-	float kD = 4.0f;
-	
-	float lastR = 0.0f;
-	float lastP = 0.0f;
-	float integR = 0.0f;
-	float integP = 0.0f;
-	
-	if(pidStoreObject.CustomData!="") {
-		var list = pidStoreObject.CustomData.Split(',');
-		lastR = float.Parse(list[0]);
-		lastP = float.Parse(list[1]);
-		// no Yaw
-		integR = float.Parse(list[3]);
-		integP = float.Parse(list[4]);
-		// no Yaw
-	} else {
-		pidStoreObject.CustomData = "0.0,0.0,0.0,0.0,0.0,0.0";
-	}
-
 	double _roll, _pitch;
 	getRollPitch(out _roll, out _pitch, vRef);
 	float roll = (float)_roll; float pitch = (float)_pitch;
 
-	float eR = targetRoll-(float)roll;
-	float eP = targetPitch-(float)pitch;
-
-	integR += eR*dt;
-	integP += eP*dt;
-
-	float eR_old = targetRoll - lastR; // TODO: store lastER instead lastR
-	float eP_old = targetPitch - lastP;
-
-	float dR = (eR - eR_old)/dt;
-	float dP = (eP - eP_old)/dt;
-
-	integR = trim(integR,2.0f);
-	integP = trim(integP,2.0f);
-
-	string storeInfo = roll.ToString() + "," + pitch.ToString() + ",0.0," + integR + "," + integP + ",0.0";
-	pidStoreObject.CustomData = storeInfo;
-
-	float controlR = kP*eR + kD*dR + kI*integR;
-	float controlP = kP*eP + kD*dP + kI*integP;
+	double controlR = pidRoll.update(roll, targetRoll);
+	double controlP = pidPitch.update(pitch, targetPitch);
 	//controlR = trim(controlR,10.0f);
 	//controlP = trim(controlP,10.0f);
 	return new Vector3D(controlR, controlP, 0.0);
@@ -165,7 +151,7 @@ void updateToTarget(Vector3D targetPos)
 	}
 	
 	// thruster control
-	//double cosV = v.Dot();
+	//double cosV = v.Dot(vTarget);
 	
 	float thrusterOverrideVal = 1.0f;
 	//if(curSpeed > 95.0) thrusterOverrideVal = 0.1f;
